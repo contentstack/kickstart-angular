68dd3bd9551d0801ac111995e7a931c9d7662af7
Tim Benniks <tbenniks@gmail.com>
2025-06-26 16:53:39 +0200

    feat: initial commit

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  contentstack: {
+    apiKey: 'blte766efb491f96715',
+    deliveryToken: '***REMOVED***',
+    previewToken: '***REMOVED***',
+    environment: 'preview',
+    region: 'EU',
+    preview: true,
+  }
+};
