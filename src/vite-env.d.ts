24562e6a035b61739d7b3f0668a82650999deb25
Tim Benniks <tbenniks@gmail.com>
2025-08-08 21:35:17 +0200

    feat: enhance environment configuration with additional Contentstack properties

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,6 +7,9 @@ interface ImportMetaEnv {
   readonly NG_APP_CONTENTSTACK_ENVIRONMENT: string;
   readonly NG_APP_CONTENTSTACK_REGION: string;
   readonly NG_APP_CONTENTSTACK_PREVIEW: string;
+  readonly NG_APP_CONTENTSTACK_CONTENT_DELIVERY: string;
+  readonly NG_APP_CONTENTSTACK_CONTENT_APPLICATION: string;
+  readonly NG_APP_CONTENTSTACK_PREVIEW_HOST: string;
 }
 
 interface ImportMeta {
