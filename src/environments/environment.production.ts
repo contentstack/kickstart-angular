b9f1d75c3468fe5defc3f9030427297f157ccadf
Tim Benniks <tbenniks@gmail.com>
2025-08-14 13:23:06 +0200

    package lock fixed and ignores adapted

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  contentstack: {
+    apiKey: '',
+    deliveryToken: '',
+    previewToken: '',
+    environment: '',
+    region: '',
+    preview: false,
+
+    contentDelivery: '',
+    previewHost: '',
+    applicationHost: ''
+  }
+};
